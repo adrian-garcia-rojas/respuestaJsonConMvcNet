c35005f1171a76351598f8877321149dcc87c8af
adrian garcia rojas <adrian_gar016@hotmail.es>
2020-02-06 21:33:37 -0600

    Se obtiene la informacion mediante un controlador y se pasa a una vista del mvc

---
+++ respuestaJsonConMvcNet/Controllers/RespuestaJsonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using respuestaJsonConMvcNet.Models;
+
+namespace respuestaJsonConMvcNet.Controllers
+{
+    public class RespuestaJsonController : Controller
+    {
+        // GET: RespuestaJson
+        public ActionResult Index()
+        {
+            List<personas> lista;
+            using (dbCursosEntities db = new dbCursosEntities())
+            {
+                lista = (from d in db.personas
+                            select d).ToList();
+            }
+
+                return View(lista);
+        }
+    }
+}
